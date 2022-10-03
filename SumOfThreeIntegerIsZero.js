17916a42a4d7922e3a961d22d6928dd575c47b62
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2022-10-03 20:38:53 +0530

    Added: Sum of three integer in array adds to zero

---
+++ SumOfThreeIntegerIsZero.js
@@ -0,0 +1,8 @@
+const ARRAY_ELEMENTS = [1, 0, -1];
+let sum = 0;
+
+for (let i = 0; i < ARRAY_ELEMENTS.length; i++) {
+    sum += ARRAY_ELEMENTS[i];
+}
+console.log("Sum of array elemnts is:");
+console.log(sum);
